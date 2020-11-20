4eb2510d32defe5078fb24883a5017d273f0917e
Ivan Likov <TheLikov2003@gmail.com>
2020-11-20 16:01:52 +0200

    Added Chapter_03 and task 01 #8

---
+++ Chapter_03/Zadacha_01/src/Zadacha_01.cpp
@@ -0,0 +1,28 @@
+//============================================================================
+// Name        : Zadacha_01.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+
+#include "head.h"
+using namespace std;
+
+int main() {
+  int intVar=0;
+  char chVar=' ';
+  float floatVar=0.0;
+
+  void_Func();
+  cout << endl ;
+  ch_Func(chVar);
+  cout << endl ;
+  int_Func(intVar);
+  cout << endl ;
+  float_Func(floatVar);
+	return 0;
+}
+
