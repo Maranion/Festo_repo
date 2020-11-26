a0b9e582c5f6f83991743a827dc9536dcebd8dae
Ivan Likov <TheLikov2003@gmail.com>
2020-11-26 13:02:07 +0200

    Added Zadacha_04.cpp chapter 3 #13

---
+++ Zadacha_04/src/Zadacha_04.cpp
@@ -0,0 +1,66 @@
+//============================================================================
+// Name        : Zadacha_04.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	char input;
+	bool quit = false;
+
+	while(true){
+		cout << "Main Menu" << endl ;
+		cout << "l: left, r: right, q: quit ->" << endl ;
+		cin >> input;
+
+		switch(input){
+		case 'l':
+			cout  << "Left Menu" << endl;
+			cout << "select a or b:" << endl;
+			cin >> input;
+			switch(input){
+			case 'a':
+				cout << "you chose a"<< endl;;
+				break;
+			case 'b':
+				cout << "you chose b"<<  endl;
+				break;
+			default:
+				cout << "You did't chose a or b" << endl;
+			}
+			break;
+
+
+		case 'r':
+			cout  << "Right Menu" << endl;
+			cout << "select c or d:" << endl;
+			cin >> input;
+			switch(input){
+			case 'c':
+			cout << "you chose c"<< endl;;
+		    break;
+			case 'd':
+			cout << "you chose d"<<  endl;
+			break;
+		    default:
+			cout << "You did't chose c or d" << endl;
+			}
+			break;
+
+		case'q':
+			quit = true;
+			break;
+		default:
+			cout << "You did't chose a correct option" << endl;
+		}
+		if(quit){
+			break;
+		}
+	}
+	return 0;
+}
