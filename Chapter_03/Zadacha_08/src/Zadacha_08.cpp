1855e97589373e2671e039617b5b2fa0f7b24ba0
Ivan Likov <TheLikov2003@gmail.com>
2020-11-27 14:34:36 +0200

    Added Zadacha_08.cpp #17

---
+++ Chapter_03/Zadacha_08/src/Zadacha_08.cpp
@@ -0,0 +1,23 @@
+//============================================================================
+// Name        : Zadacha_08.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << " ??= " << endl;
+	cout << " \??/ " << endl;
+	cout << " ??' " << endl;
+	cout << " ??( " << endl;
+	cout << " ??) " << endl;
+	cout << " ??! " << endl;
+	cout << " ??< " << endl;
+	cout << " ??> " << endl;
+	cout << " ??- " << endl;
+	return 0;
+}
