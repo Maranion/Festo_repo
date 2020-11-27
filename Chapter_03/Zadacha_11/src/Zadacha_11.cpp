368c8646728b2209b8a3ad1d7fc275e43cc55179
Ivan Likov <TheLikov2003@gmail.com>
2020-11-27 15:15:32 +0200

    Added Zadacha_11.cpp #20

---
+++ Chapter_03/Zadacha_11/src/Zadacha_11.cpp
@@ -0,0 +1,31 @@
+//============================================================================
+// Name        : Zadacha_11.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	double i,j;
+	 cout << "Enter:";
+	    cin >> i;
+	    cout << "Enter: ";
+	    cin >> j;
+
+	    cout << "i > j = " << (i > j) << endl;
+	    cout << "i < j = " << (i < j) << endl;
+	    cout << "i >= j = " << (i >= j) << endl;
+	    cout << "i <= j = " << (i <= j) << endl;
+	    cout << "i == j = " << (i == j) << endl;
+	    cout << "i != j = " << (i != j) << endl;
+	    cout << "i && j = " << (i && j) << endl;
+	    cout << "i || j = " << (i || j) << endl;
+	    cout << " (i < 10) && (j < 10) = "<< ((i < 10) && (j < 10)) << endl;
+
+	return 0;
+}
