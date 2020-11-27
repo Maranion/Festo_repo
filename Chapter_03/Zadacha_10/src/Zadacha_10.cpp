37af61064122d23a94e1e969dd19a94aee52f1ff
Ivan Likov <TheLikov2003@gmail.com>
2020-11-27 15:06:33 +0200

    Added Zadacha_10.cpp #19

---
+++ Chapter_03/Zadacha_10/src/Zadacha_10.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : Zadacha_10.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+static int fs;
+
+int main()
+{
+    fs = 1;
+
+    return 0;
+}
---
+++ Chapter_03/Zadacha_10/src/file2.cpp
@@ -0,0 +1,15 @@
+/*
+ * file2.cpp
+ *
+ *  Created on: 27.11.2020 ã.
+ *      Author: ivan
+ */
+
+
+extern int fs;
+
+void func()
+{
+    fs = 100;
+}
+
