d3d1cbf236c313c0615bf27257dfe0a2d84b3dbf
Ivan Likov <TheLikov2003@gmail.com>
2020-12-03 15:51:15 +0200

    Added Zadacha_16.cpp #25

---
+++ Chapter_03/Zadacha_16/src/Zadacha_16.cpp
@@ -0,0 +1,35 @@
+//============================================================================
+// Name        : Zadacha_16.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+enum Colors {
+	BLACK,
+	PINK,
+	RED,
+	BLUE,
+	GREEN,
+	BROWN,
+	WHITE,
+	COUNT
+};
+
+int main() {
+
+	string colorNames[Colors::COUNT] = {"Black","Pink","Red","Blue","Green","Brown","White"};
+    Colors colors[Colors::COUNT] = {BLACK,PINK,RED,BLUE,GREEN,BROWN,WHITE};
+
+    for(int i=0;i< Colors::COUNT;i++){
+    	cout << colorNames[i] << " == " << colors[i] << endl;
+    }
+
+
+
+	return 0;
+}
