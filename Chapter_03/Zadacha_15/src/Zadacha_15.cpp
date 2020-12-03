8e0d3a125a1327388de4c8a517a8b91f42ddc2b2
Ivan Likov <TheLikov2003@gmail.com>
2020-12-03 15:29:25 +0200

    Added Zadacha_15.cpp #24

---
+++ Chapter_03/Zadacha_15/src/Zadacha_15.cpp
@@ -0,0 +1,41 @@
+//============================================================================
+// Name        : Zadacha_15.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+#define PRINT(EXP) std::cout << #EXP " = " << EXP << std::endl;
+
+ typedef struct {
+	 string str1;
+	 string str2;
+	 int n;
+ }MyStruct;
+
+int main() {
+
+MyStruct myStruct;
+myStruct.str1 = "string 1";
+myStruct.str2 = "string 2";
+myStruct.n = 10;
+
+PRINT(myStruct.str1);
+PRINT(myStruct.str2);
+PRINT(myStruct.n);
+
+MyStruct* pMyStruct = &myStruct;
+pMyStruct->str1 = "string 1 is different";
+pMyStruct->str2 = "string 2 is different";
+pMyStruct->n = 1220;
+
+PRINT(pMyStruct->str1);
+PRINT(pMyStruct->str2);
+PRINT(pMyStruct->n);
+
+	return 0;
+}
