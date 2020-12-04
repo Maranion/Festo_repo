437f77af3c6e32cdbe1de3c07a6420c9b55ace5a
Ivan Likov <TheLikov2003@gmail.com>
2020-12-04 16:20:14 +0200

    fixed spacing #18

--- Chapter_03/Zadacha_09/src/static.cpp
+++ Chapter_03/Zadacha_09/src/static.cpp
@@ -8,18 +8,18 @@
 
 #include <iostream>
 using namespace std;
-	void func()
-	{
-	    static int i = 0;
-	    cout << "i= " << ++i << endl;
-	}
+    void func()
+    {
+        static int i = 0;
+        cout << "i= " << ++i << endl;
+    }
 
-	int main()
-	{
-	    for (int x = 0; x < 10; x++)
-	    {
-	        func();
-	    }
+    int main()
+    {
+        for (int x = 0; x < 10; x++)
+        {
+            func();
+        }
 
-	    return 0;
-	}
+        return 0;
+    }
