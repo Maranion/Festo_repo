a89672ff829449bb0fe3a67b26365ec034d29867
Ivan Likov <TheLikov2003@gmail.com>
2020-12-04 14:06:49 +0200

    Did task 22 #31

---
+++ Chapter_03/Zadacha_22/src/argsToDoubles.cpp
@@ -0,0 +1,22 @@
+//============================================================================
+// Name        : Zadacha_22.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char* argv[]) {
+
+for (int i = 1; i < argc; i++)
+ {
+     cout << atof(argv[i]) << endl;
+ }
+
+
+} ///:~
