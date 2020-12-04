33481cd7c37cfc21be2bfe85d73ea7186ee3db13
Ivan Likov <TheLikov2003@gmail.com>
2020-12-04 14:12:44 +0200

    Added Zadacha_23.cpp #32

---
+++ Chapter_03/Zadacha_23/src/Zadacha_23.cpp
@@ -0,0 +1,36 @@
+//============================================================================
+// Name        : Zadacha_23.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+union Primitives {
+char c;
+short s;
+int i;
+long l;
+float f;
+double d;
+long double ld;
+};
+
+
+
+int main() {
+
+Primitives p[10];
+
+Primitives* pp = p;
+
+cout << "sizeof(Primitives) = " << sizeof(Primitives) << endl;
+
+cout << "pp = " << (long)pp << endl;
+pp++;
+cout << "pp = " << (long)pp << endl;
+
+} ///:~
