67395c9f6bd9044916e57dda58b78498076468b6
Ivan Likov <TheLikov2003@gmail.com>
2020-12-04 13:54:03 +0200

    fixed name

---
+++ Chapter_03/Zadacha_18/src/Zadacha_18.cpp
@@ -0,0 +1,31 @@
+//============================================================================
+// Name        : Zadacha_18.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+#define PRINT_ADDRESS(EXP) std::cout << #EXP" -> " << (long)EXP << endl;
+
+int main()
+{
+    int arr1[4]{3};
+    char ch = '1';
+    int arr2[2]{2};
+
+    PRINT_ADDRESS(arr1)
+    PRINT_ADDRESS(&ch);
+    PRINT_ADDRESS(arr2);
+
+   cout << "First index of arr1: " << arr1[0] << endl;
+   cout << "Putting a value one index after the end of arr2" << endl;
+   arr2[2] = 22;
+   cout << "First index of arr1: " << arr1[0] << endl;
+
+
+	return 0;
+}
