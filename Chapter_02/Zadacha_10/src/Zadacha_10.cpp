0d21b349a472f3e9b4a990d8498c47f11781576d
Ivan Likov <TheLikov2003@gmail.com>
2020-12-10 10:28:28 +0200

    Fixed issue (was a mistype) #10

--- Chapter_02/Zadacha_10/src/Zadacha_10.cpp
+++ Chapter_02/Zadacha_10/src/Zadacha_10.cpp
@@ -1,11 +1,3 @@
-//============================================================================
-// Name        : Zadacha_10.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
 #include <iostream>
 #include <cstdlib>
 #include <vector>
@@ -15,17 +7,20 @@ using namespace std;
 
 int main() {
 	srand((unsigned int)time(NULL));
-			vector<float> vect;
-			float a =5.0;
+	vector<float> vect;
+	float a =5.0;
 
-			for(int i=0;i<25;i++){
-				vect.push_back((float(rand())/float((RAND_MAX)) * a));
-				cout << vect.at(i) << "|";
-				vect.at(i) =vect.at(i)*=vect.at(i);
-			}
-			cout << endl;
-					for(int i=0;i<25;i++){
-						cout <<vect.at(i) << "|";
-						}
+	for(int i=0;i<25;i++){
+		vect.push_back((float(rand())/float((RAND_MAX)) * a));
+		cout << vect.at(i) << "|";
+		vect.at(i)*=vect.at(i);
+		}
+	
+	cout << endl;
+	for(int i=0;i<25;i++){
+		cout <<vect.at(i) << "|";
+		}
+	
+	
 	return 0;
 }
