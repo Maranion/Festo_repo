4e0dbf305824d3da1327762036e4dbd4218bbb01
Ivan Likov <TheLikov2003@gmail.com>
2020-12-11 11:24:25 +0200

    Fixed spacing #21

--- Chapter_03/Zadacha_12/src/Boolean.cpp
+++ Chapter_03/Zadacha_12/src/Boolean.cpp
@@ -15,23 +15,23 @@ using namespace std;
 int main()
 {
 
-	    int i, j;
+	int i, j;
 
-	    std::cout << "Enter an integer: ";
-	    std::cin >> i;
-	    std::cout << "Enter another integer: ";
-	    std::cin >> j;
+	cout << "Enter an integer: ";
+	cin >> i;
+	cout << "Enter another integer: ";
+	cin >> j;
 
-	    std::cout << "i > j is " << (i > j) << std::endl;
-	    std::cout << "i < j is " << (i < j) << std::endl;
-	    std::cout << "i >= j is " << (i >= j) << std::endl;
-	    std::cout << "i <= j is " << (i <= j) << std::endl;
-	    std::cout << "i == j is " << (i == j) << std::endl;
-	    std::cout << "i not_eq j is " << (i not_eq j) << std::endl;
-	    std::cout << "i and j is " << (i and j) << std::endl;
-	    std::cout << "i or j is " << (i or j) << std::endl;
-	    std::cout << " (i < 10) and (j < 10) is "
-	              << ((i < 10) and (j < 10)) << std::endl;
+	cout << "i > j is " << (i > j) << std::endl;
+	cout << "i < j is " << (i < j) << std::endl;
+	cout << "i >= j is " << (i >= j) << std::endl;
+	cout << "i <= j is " << (i <= j) << std::endl;
+	cout << "i == j is " << (i == j) << std::endl;
+	cout << "i not_eq j is " << (i not_eq j) << std::endl;
+	cout << "i and j is " << (i and j) << std::endl;
+	cout << "i or j is " << (i or j) << std::endl;
+	cout << " (i < 10) and (j < 10) is "
+	<< ((i < 10) and (j < 10)) << std::endl;
 
-	    return 0;
+	return 0;
 }
