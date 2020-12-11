8ea9ccbe90d1923b12b25674d5ec8351bf131530
Ivan Likov <TheLikov2003@gmail.com>
2020-12-11 15:01:04 +0200

    Added Zadacha_33.cpp #42

---
+++ Chapter_03/Zadacha_33/src/Zadacha_33.cpp
@@ -0,0 +1,40 @@
+//============================================================================
+// Name        : Zadacha_33.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+typedef float (*ptr_func) (char);
+
+float float_func(char ch){
+
+	cout << 3.14f;
+
+	return 3.14f;
+}
+
+
+ptr_func int_func(int n){
+	float (*ptr_func) (char);
+    ptr_func = float_func;
+
+    return ptr_func;
+}
+
+
+
+int main() {
+
+	ptr_func (*ptr) (int);
+
+	ptr = int_func;
+
+	cout <<  (*ptr(3)) ('b') << endl;
+
+	return 0;
+}
