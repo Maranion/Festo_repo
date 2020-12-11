1f110aabee948d594fad9517e362402db7167bb8
Ivan Likov <TheLikov2003@gmail.com>
2020-12-11 11:39:42 +0200

    Fixed spacing #22

--- Chapter_03/Zadacha_13/src/Rotation.cpp
+++ Chapter_03/Zadacha_13/src/Rotation.cpp
@@ -7,56 +7,57 @@
 
 #include <iostream>
 using namespace std;
+
 extern void printBinary(const unsigned char val);
 
 unsigned char rol(unsigned char val)
 {
-    int highbit;
-    if (val & 0x80)
-    {
-        highbit = 1;
-    }
-    else
-    {
-        highbit = 0;
-    }
+	int highbit;
+	if (val & 0x80)
+	{
+		highbit = 1;
+	}
+	else
+	{
+		highbit = 0;
+	}
 
-   cout <<endl;
-    cout << "Took the high bit: " << highbit << endl;
+	cout <<endl;
+	cout << "Took the high bit: " << highbit << endl;
 
-    val <<= 1;
-    cout << "Shifted the number to the left: ";
-    printBinary(val);
-    cout << endl;
+	val <<= 1;
+	cout << "Shifted the number to the left: ";
+	printBinary(val);
+	cout << endl;
 
-    val |= highbit;
-    cout << "Inserted the high bit into the back of the number: ";
-    return val;
+	val |= highbit;
+	cout << "Inserted the high bit into the back of the number: ";
+	return val;
 }
 
 unsigned char ror(unsigned char val)
 {
-    int lowbit;
-    if (val & 1) // Check the low bit
-    {
-        lowbit = 1;
-    }
-    else
-    {
-        lowbit = 0;
-    }
+	int lowbit;
+	if (val & 1) // Check the low bit
+	{
+		lowbit = 1;
+	}
+	else
+	{
+		lowbit = 0;
+	}
 
-    cout << endl;
-   cout << "Took the low bit: " << lowbit << endl;
+	cout << endl;
+	cout << "Took the low bit: " << lowbit << endl;
 
-    val >>= 1;
-    cout << "Shifted the number to the left: ";
-    printBinary(val);
-    cout << endl;
+	val >>= 1;
+	cout << "Shifted the number to the left: ";
+	printBinary(val);
+	cout << endl;
 
-    val |= (lowbit << 7);
-    cout << "Inserted low bit into the front of the number: ";
-    return val;
+	val |= (lowbit << 7);
+	cout << "Inserted low bit into the front of the number: ";
+	return val;
 }
 
 
