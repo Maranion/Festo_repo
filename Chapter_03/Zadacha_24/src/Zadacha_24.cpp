3dd73be94d25d5a9bd0f4e9fab9056d8798f865c
Ivan Likov <TheLikov2003@gmail.com>
2020-12-11 12:34:47 +0200

    fixed spacing #33

--- Chapter_03/Zadacha_24/src/Zadacha_24.cpp
+++ Chapter_03/Zadacha_24/src/Zadacha_24.cpp
@@ -15,41 +15,41 @@ using namespace std;
 
 int main() {
 
-	   long longArr[SIZE];
-	    for (int i = 0; i < SIZE; i++)
-	    {
-	        longArr[i] = i; // Give it index values
-	    }
+	long longArr[SIZE];
+	for (int i = 0; i < SIZE; i++)
+	{
+		longArr[i] = i; // Give it index values
+	}
 
-	    long *longPtr = longArr;
-	    P(*longPtr);
-	    P(*++longPtr);
-	    P(*(longPtr + 5));
+	long *longPtr = longArr;
+	P(*longPtr);
+	P(*++longPtr);
+	P(*(longPtr + 5));
 
-	    long *longPtr2 = longPtr + 5;
-	    P(*longPtr2);
-	    P(*(longPtr2 - 4));
-	    P(*--longPtr2);
-	    P(longPtr2 - longPtr); // Yields number of elements
+	long *longPtr2 = longPtr + 5;
+	P(*longPtr2);
+	P(*(longPtr2 - 4));
+	P(*--longPtr2);
+	P(longPtr2 - longPtr); // Yields number of elements
 
-	    cout << endl;
+	cout << endl;
 
-	    long double doubleArr[SIZE];
-	    for (int i = 0; i < SIZE; i++)
-	    {
-	    	doubleArr[i] = i + i; // Give it index values
-	    }
+	long double doubleArr[SIZE];
+	for (int i = 0; i < SIZE; i++)
+	{
+	doubleArr[i] = i + i; // Give it index values
+	}
 
-	    long double *doublePtr = doubleArr;
-	    P(*doublePtr);
-	    P(*++doublePtr);
-	    P(*(doublePtr + 5));
+	long double *doublePtr = doubleArr;
+	P(*doublePtr);
+	P(*++doublePtr);
+	P(*(doublePtr + 5));
 
-	    long  double *doublePtr2 = doublePtr + 5;
-	    P(*doublePtr2);
-	    P(*(doublePtr2 - 4));
-	    P(*--doublePtr2);
-	    P(doublePtr2 - doublePtr); // Yields number of elements
+	long  double *doublePtr2 = doublePtr + 5;
+	P(*doublePtr2);
+	P(*(doublePtr2 - 4));
+	P(*--doublePtr2);
+	P(doublePtr2 - doublePtr); // Yields number of elements
 
 } ///:~
 
