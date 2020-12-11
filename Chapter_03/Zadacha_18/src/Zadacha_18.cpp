ea4b1218e75cd8bf7d093365fec8f244da3e2493
Ivan Likov <TheLikov2003@gmail.com>
2020-12-11 12:10:38 +0200

    Fixed spacing #27

--- Chapter_03/Zadacha_18/src/Zadacha_18.cpp
+++ Chapter_03/Zadacha_18/src/Zadacha_18.cpp
@@ -9,22 +9,22 @@
 #include <iostream>
 using namespace std;
 
-#define PRINT_ADDRESS(EXP) std::cout << #EXP" -> " << (long)EXP << endl;
+#define PRINT_ADDRESS(EXP) cout << #EXP" -> " << (long)EXP << endl;
 
 int main()
 {
-    int arr1[4]{3};
-    char ch = '1';
-    int arr2[2]{2};
+	int arr1[4]{3};
+	char ch = '1';
+	int arr2[2]{2};
 
-    PRINT_ADDRESS(arr1)
-    PRINT_ADDRESS(&ch);
-    PRINT_ADDRESS(arr2);
+	PRINT_ADDRESS(arr1)
+	PRINT_ADDRESS(&ch);
+	PRINT_ADDRESS(arr2);
 
-   cout << "First index of arr1: " << arr1[0] << endl;
-   cout << "Putting a value one index after the end of arr2" << endl;
-   arr2[2] = 22;
-   cout << "First index of arr1: " << arr1[0] << endl;
+	cout << "First index of arr1: " << arr1[0] << endl;
+	cout << "Putting a value one index after the end of arr2" << endl;
+	arr2[2] = 22;
+	cout << "First index of arr1: " << arr1[0] << endl;
 
 
 	return 0;
