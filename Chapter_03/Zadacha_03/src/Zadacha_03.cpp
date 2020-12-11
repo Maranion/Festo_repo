514fb820d85a47aa73efa70a213df3a13a6a4dbb
Ivan Likov <TheLikov2003@gmail.com>
2020-12-11 10:15:02 +0200

    fixed spacing #12

--- Chapter_03/Zadacha_03/src/Zadacha_03.cpp
+++ Chapter_03/Zadacha_03/src/Zadacha_03.cpp
@@ -10,45 +10,43 @@
 using namespace std;
 
 int main() {
-    string input;
-    int choise =0;
-    bool quit;
+	string input;
+	int choise =0;
+	bool quit;
 
-    while(true){
-    cout << "quick a dog is chasing you choose: run,fight,quit"<<endl;
-    cin >> input;
+	while(true){
+		cout << "quick a dog is chasing you choose: run,fight,quit"<<endl;
+		cin >> input;
 
-    if(input=="run"){
+		if(input=="run"){
+			choise =1;
+		}else if(input=="fight"){
+			choise = 2;
+		}else if(input=="quit"){
+			choise = 3;
+		}else{
+			choise =0;
+		}
 
-        choise =1;
-    }else if(input=="fight"){
+		switch(choise){
+			case 1:
+				cout << "lol you can't out run a dog, he catches up and bites you. Ouch" << endl;
+				break;
+			case 2:
+				cout << "you turn around and kick the dog... It's a cihuahua... You are  reported for animal cruelty." << endl;
+				break;
+			case 3:
+				quit = true;
+				cout << "you turn around and kick the dog... It's a cihuahua... You are  reported for animal cruelty." << endl;
+			default:
+				cout << "invalid input" << endl;
+		}
 
-        choise = 2;
-    }else if(input=="quit"){
-        choise = 3;
-        }else{
-        choise =0;
-    }
+		if(quit){
+		break;
+		}
 
-    switch(choise){
-        case 1:
-            cout << "lol you can't out run a dog, he catches up and bites you. Ouch" << endl;
-            break;
-        case 2:
-            cout << "you turn around and kick the dog... It's a cihuahua... You are  reported for animal cruelty." << endl;
-            break;
-        case 3:
-            quit = true;
-            cout << "you turn around and kick the dog... It's a cihuahua... You are  reported for animal cruelty." << endl;
-        default:
-            cout << "invalid input" << endl;
-    }
+	}
 
-    if(quit){
-        break;
-    }
-
-    }
-
-    return 0;
+	return 0;
 }
