e0560e0719463b598b71267063b7dcda811a146e
Ivan Likov <TheLikov2003@gmail.com>
2020-12-11 11:14:25 +0200

    Fixed spacing #18

--- Chapter_03/Zadacha_09/src/nonStatic.cpp
+++ Chapter_03/Zadacha_09/src/nonStatic.cpp
@@ -7,21 +7,19 @@
 
 #include <iostream>
 using namespace std;
-    void func()
-    {
-        int i = 0;
-        cout << "i= " << ++i << endl;
-    }
+void func()
+{
+	int i = 0;
+	cout << "i= " << ++i << endl;
+}
 
-    int main()
-    {
-        for (int x = 0; x < 10; x++)
-        {
-            func();
-        }
+int main()
+{
+	for (int x = 0; x < 10; x++)
+	{
+		func();
+	}
 
-        return 0;
-    }
-
-
+	return 0;
+}
 
