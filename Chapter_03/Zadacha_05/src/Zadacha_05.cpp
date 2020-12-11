fc43385bc8c7a4b1270387212529479cb83f87d6
Ivan Likov <TheLikov2003@gmail.com>
2020-12-11 11:02:33 +0200

    Fixed spacing #15

--- Chapter_03/Zadacha_05/src/Zadacha_05.cpp
+++ Chapter_03/Zadacha_05/src/Zadacha_05.cpp
@@ -9,19 +9,20 @@
 #include <iostream>
 using namespace std;
 
-#define PRINT(EXP)\
-{\
-cout << "A = "  << #EXP << endl;\
-cout << "A = " << EXP << endl;\
-}
+#define PRINT(EXP)					\
+{									\
+cout << "A = "  << #EXP << endl;	\
+cout << "A = " << EXP << endl;		\
+}									\
+
 int main() {
 
- int x=1,y=2,z=3;
+	int x=1,y=2,z=3;
 
 
 
- PRINT(x + y - 2/2 + z);
- PRINT(x + (y - 2) / (2 + z));
+ 	PRINT(x + y - 2/2 + z);
+ 	PRINT(x + (y - 2) / (2 + z));
 
-    return 0;
+	return 0;
 }
