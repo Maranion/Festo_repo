34e06d05d73ac14789251003be13cdbbe8bf0cd0
Ivan Likov <TheLikov2003@gmail.com>
2020-12-17 13:45:50 +0200

    Fixed code format #9

--- Chapter_02/Zadacha_09/src/Zadacha_09.cpp
+++ Chapter_02/Zadacha_09/src/Zadacha_09.cpp
@@ -14,21 +14,21 @@ int main() {
 		vect.push_back((float(rand())/float((RAND_MAX)) * a));
 		vect2.push_back((float(rand())/float((RAND_MAX)) * a));
 		vect3.push_back(vect.at(i)+vect2.at(i));
-		}
+	}
 
 	for(int i=0;i<25;i++){
 		cout <<vect.at(i) << "|";
-		}
+	}
 
 	cout << endl;
 	for(int i=0;i<25;i++){
 		cout <<vect2.at(i) << "|";
-		}
+	}
 
 	cout << endl;
 	for(int i=0;i<25;i++){
 		cout <<vect3.at(i) << "|";
-		}
+	}
 
 	return 0;
 }
