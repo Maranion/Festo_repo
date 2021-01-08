2dc13abe48673a1ee1eab7b18805a0b324efe07f
Ivan Likov <TheLikov2003@gmail.com>
2021-01-08 15:48:17 +0200

    Did task 1 #44

---
+++ Chapter_04/Zadacha_01/src/Zadacha_01.c
@@ -0,0 +1,13 @@
+/*
+ ============================================================================
+ Name        : Zadacha_01.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+}
