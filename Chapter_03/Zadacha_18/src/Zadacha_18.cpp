76bfb593542e187b7ab32c80d3160bad4c124a94
Ivan Likov <TheLikov2003@gmail.com>
2021-01-14 14:50:40 +0200

    removed magic numbers #27

--- Chapter_03/Zadacha_18/src/Zadacha_18.cpp
+++ Chapter_03/Zadacha_18/src/Zadacha_18.cpp
@@ -13,9 +13,12 @@ using namespace std;
 
 int main()
 {
-	int arr1[4]{3};
-	char ch = '1';
-	int arr2[2]{2};
+    const int SIZE_ONE = 4;
+	const int SIZE_TWO = SIZE_ONE / 2;
+
+	int arr1[SIZE_ONE]{3};
+	char ch = 'A';
+	int arr2[SIZE_TWO]{2};
 
 	PRINT_ADDRESS(arr1)
 	PRINT_ADDRESS(&ch);
@@ -23,7 +26,7 @@ int main()
 
 	cout << "First index of arr1: " << arr1[0] << endl;
 	cout << "Putting a value one index after the end of arr2" << endl;
-	arr2[2] = 22;
+	arr2[SIZE_TWO] = INT32_MAX;
 	cout << "First index of arr1: " << arr1[0] << endl;
 
 
