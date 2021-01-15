3d4b172a951fbf901fd1e2e702f720d47b6a2304
Ivan Likov <TheLikov2003@gmail.com>
2021-01-15 09:40:22 +0200

    Fixed formatting and mistakes #31

--- Chapter_03/Zadacha_22/src/argsToDoubles.cpp
+++ Chapter_03/Zadacha_22/src/argsToDoubles.cpp
@@ -8,15 +8,14 @@
 
 #include <iostream>
 #include <cstdlib>
-using namespace std;
 
+using namespace std;
 
 int main(int argc, char* argv[]) {
 
-for (int i = 1; i < argc; i++)
- {
-	cout << atof(argv[i]) << endl;
- }
+	for (int i = 1; i < argc; i++) {
+		cout << atof(argv[i]) << endl;
+ 	}
 
-
+return 0;
 } ///:~
--- Chapter_03/Zadacha_22/src/argsToLongs.cpp
+++ Chapter_03/Zadacha_22/src/argsToLongs.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <cstdlib>
+
 using namespace std;
 
 
 int main(int argc, char* argv[]) {
 
-	for(int i = 1; i < argc; i++){
+	for(int i = 1; i < argc; i++) {
 		cout << atol(argv[i]) << endl;
 	}
 
+return 0;
 } ///:~
