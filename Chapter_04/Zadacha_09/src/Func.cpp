ed7d434ab280ce873836258a173d9c895e1a4282
Ivan Likov <TheLikov2003@gmail.com>
2021-01-22 15:25:50 +0200

    Added Zadacha_09.cpp #51

---
+++ Chapter_04/Zadacha_09/src/Func.cpp
@@ -0,0 +1,11 @@
+/*
+ * Func.cpp
+ *
+ *  Created on: 22.01.2021 ã.
+ *      Author: ivan
+ */
+#include "stdio.h"
+
+void f(int i) {
+	printf("%d\n", i);
+}
---
+++ Chapter_04/Zadacha_09/src/Zadacha_09.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : Zadacha_09.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include "stdio.h"
+
+void f(float i) {
+	printf("%.2f\n", i);
+}
+
+int main() {
+
+	f(3.14);
+
+	return 0;
+}
