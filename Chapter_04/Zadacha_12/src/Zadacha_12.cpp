f3b20c9d7164acbfe452e665b0d026b43bbc3763
Ivan Likov <TheLikov2003@gmail.com>
2021-01-22 16:16:27 +0200

    I see no difference #53

---
+++ Chapter_04/Zadacha_12/src/Zadacha_12.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : Zadacha_12.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+
+int main() {
+
+	assert(0);
+	cout << "hi";
+
+	return 0;
+}
