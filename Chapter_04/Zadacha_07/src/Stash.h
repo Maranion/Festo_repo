31cc7e759734d772ef7a2dfb3acf539a16ca85ed
Ivan Likov <TheLikov2003@gmail.com>
2021-01-22 13:55:13 +0200

    Added Zadacha_07.cpp #49

---
+++ Chapter_04/Zadacha_07/src/Stash.h
@@ -0,0 +1,28 @@
+/*
+ * Stash.h
+ *
+ *  Created on: 15.01.2021 ã.
+ *      Author: ivan
+ */
+
+#ifndef STASH_H_
+#define STASH_H_
+
+struct Stash {
+
+	int size;      // Size of each space
+	int quantity;  // Number of storage spaces
+	int next;      // Next empty space
+
+  // Dynamically allocated array of bytes:
+	unsigned char* storage;
+
+    void initialize(int size);
+    void cleanup();
+	int add(const void* element);
+	void* fetch(int index);
+	int count();
+	void inflate(int increase);
+};
+
+#endif /* STASH_H_ */
