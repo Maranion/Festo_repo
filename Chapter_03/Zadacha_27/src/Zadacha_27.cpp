dc786e6690b3348f30777b892339ae8f00a0477b
Ivan Likov <TheLikov2003@gmail.com>
2021-02-12 10:10:53 +0200

    fixed #36

--- Chapter_03/Zadacha_27/src/Zadacha_27.cpp
+++ Chapter_03/Zadacha_27/src/Zadacha_27.cpp
@@ -9,28 +9,27 @@
 #include <iostream>
 using namespace std;
 
-#define PRINT(ARR,SIZE)			\
-	cout << #ARR << "[";		\
-	for(int i=0;i<SIZE;i++){	\
-		cout << ARR[i];			\
-		cout << "|";			\
-}								\
-cout << "]" << endl;			\
+#define PRINT(ARR, SIZE)				\
+	cout << #ARR << "[";				\
+	for(int i = 0; i < SIZE; i++) {		\
+		cout << ARR[i];					\
+		cout << " | ";					\
+	}									\
+	cout << "]" << endl;				\
 
 int main() {
 	int SIZE = 6;
 
-	const double cArr[SIZE]{ 70.27,15.43,58.41,96.89,50.04,24.42};
-	volatile double vArr[SIZE]{46.38,1.11,2.7,82.8,48.79,36.55};
+	const double cArr[SIZE]{70.27, 15.43, 58.41, 96.89, 50.04, 24.42};
+	volatile double vArr[SIZE]{46.38, 1.11, 2.7, 82.8, 48.79, 36.55};
 
-	for(int i=0;i<SIZE;i++){
+	for(int i = 0; i < SIZE; i++) {
 		const_cast<double*>(cArr)[i] *= 2;
 		const_cast<double*>(vArr)[i] *= 2;
 	}
 
-	PRINT(cArr,SIZE);
-	PRINT(vArr,SIZE);
-
+	PRINT(cArr, SIZE);
+	PRINT(vArr, SIZE);
 
 	return 0;
 }
