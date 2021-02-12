ec337959dc976b319f9c3b57ca306655963821de
Ivan Likov <TheLikov2003@gmail.com>
2021-02-12 09:31:05 +0200

    Fixed formatting #28

--- Chapter_03/Zadacha_19/src/Zadacha_19.cpp
+++ Chapter_03/Zadacha_19/src/Zadacha_19.cpp
@@ -2,32 +2,30 @@
 
 
 #include <iostream>
-
 using namespace std;
 
-
 #define PRINT_ARR(TYPE,ARR)											\
-	for(int i = 0; i < 10; i++){									\
+	for(int i = 0; i < 10; i++) { 									\
 		cout << "&arr[" << i << "] = "<< (long)&ARR[i] << endl;		\
 	}																\
 	cout << endl;													\
 
-int main()
-{
+int main() {
+	const int SIZE = 10;
 
-	int intArr[10];
-	PRINT_ARR(int,intArr);
+	int intArr[SIZE];
+	PRINT_ARR(int, intArr);
 
-	char charArr[10];
-	PRINT_ARR(char,charArr);
+	char charArr[SIZE];
+	PRINT_ARR(char, charArr);
 
-	long longArr[10];
-	PRINT_ARR(long,longArr);
+	long longArr[SIZE];
+	PRINT_ARR(long, longArr);
 
-	float floatArr[10];	
-	PRINT_ARR(float,floatArr);
+	float floatArr[SIZE];	
+	PRINT_ARR(float, floatArr);
 
-	double doubleArr[10];
-	PRINT_ARR(double,doubleArr);
+	double doubleArr[SIZE];
+	PRINT_ARR(double, doubleArr);
 
 } ///:~
